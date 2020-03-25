c35248633150025a8af048d5fe533079789009f0
hien.nguyen <hien.nguyen@elisa.fi>
2020-03-25 16:17:44 +0200

    add test

---
+++ hadoop-cloud-deployment/countryFilter-EMR.hql
@@ -0,0 +1,24 @@
+CREATE EXTERNAL TABLE HDI(
+	id INT, country STRING, hdi FLOAT, lifeex INT, mysch INT, eysch INT, gni INT
+	) 
+	ROW FORMAT DELIMITED 
+	FIELDS TERMINATED BY ',' 
+	STORED AS TEXTFILE
+	LOCATION '${INPUT}';
+
+CREATE EXTERNAL TABLE output_countries(
+    country STRING, gni INT
+    )    
+    ROW FORMAT DELIMITED
+    FIELDS TERMINATED BY ','    
+    STORED AS TEXTFILE
+    LOCATION '${OUTPUT}'
+  ;
+
+INSERT OVERWRITE TABLE output_countries
+	SELECT 
+		country, gni 
+	FROM 
+		HDI 
+	WHERE 
+		gni > 2000;
---
+++ hadoop-cloud-deployment/test-hadoop/src/main/java/WordCountWithTools.java
@@ -0,0 +1,48 @@
+
+import java.util.Arrays;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+
+
+public class WordCountWithTools extends Configured implements Tool {
+
+    public int run(String[] args) throws Exception {
+        if (args.length < 2) {
+            ToolRunner.printGenericCommandUsage(System.out);
+            System.out.println("");
+            return -1;
+        }
+
+        System.out.println(Arrays.toString(args));
+
+        Job job = Job.getInstance(getConf(), "word count");
+        job.setJarByClass(WordCount.class);
+        job.setMapperClass(WordCount.TokenizerMapper.class);
+        // Uncomment this to
+        // job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(WordCount.IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        job.waitForCompletion(true);
+
+        return 0;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int res = ToolRunner.run(new Configuration(), new WordCountWithTools(), args);
+        System.exit(res);
+    }
+
+}
